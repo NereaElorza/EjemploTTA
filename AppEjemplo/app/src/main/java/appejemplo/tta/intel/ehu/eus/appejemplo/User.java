a9afa7a150c87db8d4e96f266fc318b69a22ce1a
Nerea Elorza <nereae157@hotmail.com>
2017-01-13 18:05:09 +0100

    clase User creada

---
+++ AppEjemplo/app/src/main/java/appejemplo/tta/intel/ehu/eus/appejemplo/User.java
@@ -0,0 +1,29 @@
+package appejemplo.tta.intel.ehu.eus.appejemplo;
+
+/**
+ * Created by nerea on 13/01/17.
+ */
+public class User {
+    private int id;
+    private String name;
+    private int lessonNum;
+    private String lessonTit;
+    private int nextTest;
+    private int nextExercise;
+
+    public User(int id, String name, int lessonNum, String lessonTit, int nextTest, int nextExercise){
+        this.id=id;
+        this.name=name;
+        this.lessonNum=lessonNum;
+        this.lessonTit=lessonTit;
+        this.nextTest=nextTest;
+        this.nextExercise=nextExercise;
+    }
+    public void setId(){}
+    public void setName(){}
+    public void setLessonNum(){}
+    public void setLessonTit(){}
+    public void setNextTest(){}
+
+
+}
